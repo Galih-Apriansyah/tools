97682786f887fe59d4ec7f5cf810dbd624a04e86
Galih-Apriansyah <80031025+Galih-Apriansyah@users.noreply.github.com>
2021-03-05 07:41:34 +0700

    kalkulator

---
+++ Apr.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('clear')
+print '============Galih================'
+print '(1) penjumlahan'
+print '(2) pengurangan'
+print '(3) perkalian'
+print '(4) pembagian'
+print '==========Apriansyah============='
+
+plih = input('masukkan pilihan anda : ')
+if plih == 1:
+     print
+     angka1 = input('masukkan angka pertama : ')
+     angka2 = input('masukkan angka kedua : ')
+     total = angka1 + angka2
+     print'hasil penjumlahan', total
+© 
